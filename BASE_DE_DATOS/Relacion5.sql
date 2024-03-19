51dbda97a35c1399cb4eb324a318919589601663
AdrianPerez2207 <aperezsanchez441@gmail.com>
2024-03-19 18:54:27 +0100

    relacion 5

--- BASE_DE_DATOS/Relacion5.sql
+++ BASE_DE_DATOS/Relacion5.sql
@@ -94,22 +94,22 @@ insert into trabaja values
 /*Nombre de los empleados que han trabajado más de 50 horas en proyectos.*/
 select nom_emp, sum(nhoras)
 from empleado
-join trabaja on trabaja.cod_emp like empleado.cod_emp
-join proyecto on trabaja.cod_pro like proyecto.cod_pro
+join trabaja on trabaja.cod_emp = empleado.cod_emp
+join proyecto on trabaja.cod_pro = proyecto.cod_pro
 group by nom_emp
 having sum(nhoras) > 50;
 
 /*Nombre de los departamentos que tienen empleados con apellido “Verde”.*/
 select nom_dep
 from departamento as de
-join empleado as em on em.cod_dep like de.cod_dep
+join empleado as em on em.cod_dep = de.cod_dep
 where nom_emp like "% VERDE";
 
 /*Nombre de los proyectos en los que trabajan más de dos empleados*/
 select nom_pro, count(e.cod_emp)
 from proyecto as p
-join trabaja as t on p.cod_pro like t.cod_pro
-join empleado as e on t.cod_emp like e.cod_emp
+join trabaja as t on p.cod_pro = t.cod_pro
+join empleado as e on t.cod_emp = e.cod_emp
 group by p.nom_pro
 having count(e.cod_emp) > 2;
 
@@ -117,15 +117,191 @@ having count(e.cod_emp) > 2;
 deben percibir, a razón de 35 euros la hora. La lista se presentará ordenada por orden alfabético
 de nombre de empleado, y en caso de que no pertenezcan a ningún departamento (NULL) debe
 aparecer la palabra “DESCONOCIDO”.*/
+select nom_emp, ifnull(nom_dep, "DESCONOCIDO"), (sum(nhoras) * 35) as dinero 
+from empleado as e
+join trabaja as t on e.cod_emp = t.cod_emp
+left join departamento as d on e.cod_dep = d.cod_dep
+group by e.cod_emp
+order by 1 asc; 
+
+
 
 /*Lista de los nombres de todos los empleados, y el número de proyectos en los que está
 trabajando (ten en cuenta que algunos empleados no trabajan en ningúb proyecto).*/
 select nom_emp, count(p.cod_pro)
 from empleado as e
-left join trabaja as t on e.cod_emp like t.cod_emp
-left join proyecto as p on t.cod_pro like p.cod_pro
+left join trabaja as t on e.cod_emp = t.cod_emp
+left join proyecto as p on t.cod_pro = p.cod_pro
 group by nom_emp
 order by 2 asc;
+
+/*6. Lista de empleados que trabajan en Málaga o en Almería.*/
+select *
+from empleado as e
+join departamento as d on e.cod_dep = d.cod_dep
+where ciudad like "MÁLAGA"
+or ciudad like "GRANADA";
+
+/*7. Lista alfabética de los nombres de empleado y los nombres de sus jefes. Si el empleado no tiene
+jefe debe aparecer la cadena “Sin Jefe”.*/
+select e.nom_emp, ifnull(jefe.nom_emp, "Sin jefe")
+from empleado as e
+left join empleado as jefe on jefe.cod_emp = e.cod_jefe
+order by 1 desc;
+
+/*8. Fechas de ingreso mínima. y máxima, por cada departamento.*/
+select nom_dep, min(fecha_ingreso), max(fecha_ingreso)
+from empleado as e
+join departamento as d on e.cod_dep = d.cod_dep
+group by d.cod_dep
+order by 2 desc;
+
+/*9. Nombres de empleados que trabajan en el mismo departamento que Carmen Violeta.*/
+select nom_emp
+from empleado as e
+join departamento as d on e.cod_dep = d.cod_dep
+where d.cod_dep = (select cod_dep
+					from empleado as e
+                    where nom_emp like "CARMEN VIOLETA");
+
+/*10. Media del año de ingreso en la empresa de los empleados que trabjan en algún proyecto.*/
+select distinct round(avg(year(fecha_ingreso)))
+from empleado as e
+join trabaja as t on e.cod_emp = t.cod_emp;
+
+
+/*11. Nombre de los empleados que tienen de apellido Verde o Rojo, y simultáneamente su jefa es
+Esperanza Amarillo.*/
+select e.nom_emp
+from empleado as e
+join empleado as jefe on jefe.cod_emp = e.cod_jefe
+where jefe.nom_emp like "ESPERANZA AMARILLO"
+and (e.nom_emp like "% ROJO" or e.nom_emp like "% VERDE");
+
+
+/*12. Suponiendo que la letra que forma parte del código de empleado es la categoría laboral, listar los
+empleados de categoría B que trabajan en algún proyecto.*/
+select e.nom_emp, e.cod_emp
+from empleado as e
+join trabaja as t on e.cod_emp = t.cod_emp
+where e.cod_emp like "B%";
+
+
+/*13. Listado de nombres de departamento, ciudad del departamento y número de empleados del
+departamento. Ordenada por nombre de ciudad y a igualdad de esta por el nombre del
+departamento.*/
+select nom_dep, ciudad, count(e.cod_emp)
+from departamento as d
+join empleado as e on d.cod_dep = e.cod_dep
+group by d.cod_dep
+order by 2 desc, 1; /*ordenamos por nombre de ciudad y despartamento*/
+
+
+
+/*14. Lista de nombres de proyecto y suma de horas trabajadas en él, de los proyectos en los que se ha
+trabajado más horas de la media de horas trabajadas en todos los proyectos.*/
+select p.nom_pro, sum(t.nhoras)
+from proyecto as p
+join trabaja as t on p.cod_pro = t.cod_pro
+group by t.cod_pro
+having sum(t.nhoras) >= (select avg(nhoras)
+						from trabaja);
+
+
+/*15. Nombre de proyecto y horas trabajadas, del proyecto en el que más horas se ha trabajado.*/
+select p.nom_pro, sum(t.nhoras)
+from proyecto as p
+join trabaja as t on p.cod_pro = t.cod_pro
+group by t.cod_pro
+having sum(t.nhoras) = (select sum(nhoras)
+						from trabaja
+                        group by cod_pro
+						order by 1 desc limit 1);
+
+/*16. Lista de nombres de empleado que hayan trabajado entre 15 y 100 horas, entre todos los
+proyectos en los que trabajan.*/
+select nom_emp, sum(nhoras)
+from empleado as e
+join trabaja as t on e.cod_emp = t.cod_emp
+join proyecto as p on t.cod_pro = p.cod_pro
+group by e.cod_emp
+having sum(nhoras) between 15 and 100
+order by 2 asc;
+
+
+/*17. Lista de empleados que no son jefes de ningún otro empleado.*/
+select e.nom_emp
+from empleado as e
+where not exists (select nom_emp
+					from empleado as e2
+                    where e2.cod_jefe = e.cod_emp);
+
+
+/*18. Se quiere premiar a los empleados del departamento que mejor productividad tenga. Para ello se
+decide que una medida de la productividad puede ser el número de horas trabajadas por
+empleados del departamento en proyectos, dividida por los empleados de ese departamento.
+¿Qué departamento es el más productivo?*/
+select nom_dep, sum(nhoras)
+from departamento as d
+join empleado as e on d.cod_dep = e.cod_dep
+join trabaja as t on e.cod_emp = t.cod_emp
+group by d.cod_dep
+having sum(nhoras)/count(e.cod_emp) = (select sum(nhoras)/count(e.cod_emp)
+										from trabaja as t
+                                        join empleado as e on t.cod_emp = e.cod_emp
+                                        join departamento as d on e.cod_dep = d.cod_dep
+										group by d.cod_dep
+                                        order by 1 desc limit 1);
+
+
+
+
+/*19. Lista donde aparezcan los nombres de empleados, nombres de sus departamentos y nombres de
+proyectos en los que trabajan. Los empleados sin departamento, o que no trabajen en proyectos
+aparecerán en la lista y en lugar del departamento o el proyecto aparecerá “*****”.*/
+select nom_emp, ifnull(nom_dep, "*****"), ifnull(nom_pro, "*****")
+from empleado as e
+left join trabaja as t on e.cod_emp = t.cod_emp
+left join departamento as d on e.cod_dep = d.cod_dep
+left join proyecto as p on d.cod_dep = p.cod_dep
+order by 3 asc;
+
+
+
+
+/*20. Lista de los empleados indicando el número de días que llevan trabajando en la empresa.*/
+select nom_emp, datediff(now(), fecha_ingreso) /*Función para calcular los dias entre fechas*/
+from empleado
+order by 2;
+
+
+/*21. Número de proyectos en los que trabajan empleados de la ciudad de Córdoba.*/
+select count(p.cod_pro)
+from proyecto as p
+join trabaja as t on p.cod_pro = t.cod_pro
+join empleado as e on t.cod_emp = e.cod_emp
+join departamento as d on e.cod_dep = d.cod_dep
+where d.ciudad like "CÓRDOBA";
+
+
+/*22. Lista de los empleados que son jefes de más de un empleado, junto con el número de empleados
+que están a su cargo.*/
+
+
+
+
+
+/*23. Listado que indique años y número de empleados contratados cada año, todo ordenado por orden
+ascendente de año.*/
+/*24. Listar los nombres de proyectos en los que aparezca la palabra “energía”, indicando también el
+nombre del departamento que lo gestiona.*/
+/*25. Lista de departamentos que están en la misma ciudad que el departamento “Gerencia”.*/
+/*26. Lista de departamentos donde exista algún trabajador con apellido “Amarillo”.*/
+/*27. Lista de los nombres de proyecto y departamento que los gestiona, de los proyectos que tienen 0
+horas de trabajo realizadas.*/
+/*28. Asignar el empleado “Manuel Amarillo” al departamento “05”*/
+/*29. Borrar los departamentos que no tienen empleados.*/
+/*30. Añadir todos los empleados del departamento 02 al proyecto MES.*/
 
 
 
